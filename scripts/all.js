e959d2faf518f2cb0893242c3c203fcd4609180f
Joe Wong <dzinlife@me.com>
2014-08-18 17:26:29 +0800

    test

--- scripts/all.js
+++ scripts/all.js
@@ -10,15 +10,15 @@ $(document).ready(function() {
 		var input = $(".promo").eq(0).width();
 
 		$(".aside-wrapper").height(function(){	
-			return input / mapping(input, 200, 400, 1, 1.5)
+			return input / mapping(input, 256, 480, 1.4, 2)
 		});
 
 		function mapping(input, a1, a2, b1, b2) {
 			var result = (input - a1) / (a2 - a1) * (b2 - b1) + b1;
-			if (result < 1) {
-				return 1;
-			}else if (result > 1.8){
-				return 1.8;
+			if (result < b1) {
+				return b1;
+			}else if (result > b2){
+				return b2;
 			}else{
 				return result;
 			}
