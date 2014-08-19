675fa4584e9438b055f9aa875e0eb2d8b08d3e81
Joe Wong <dzinlife@me.com>
2014-08-19 18:08:37 +0800

    test

--- scripts/all.js
+++ scripts/all.js
@@ -7,34 +7,8 @@ $(document).ready(function() {
 	})
 
 	function init() {
-		// img_resize()
+		
 	}
-
-	function img_resize () {
-		if ($(window).width() > 1000) {
-			$(".main-poster").width("1600").css({
-				"margin-top": function() {
-					return -($(this).height() - ( $(".body-wrapper").height() - $(".aside-wrapper").height() ) )/2 + "px";
-				},
-				"margin-left": function() {
-					return ( $("body").width() - $(".main-poster").width() )/2
-				}
-			});
-		}else{
-			$(".main-poster").width(1200).css({
-				"margin-top": function() {
-					return -($(this).height() - ( $(".body-wrapper").height() - $(".aside-wrapper").height() ) )/2 + "px";
-				},
-				"margin-left": function() {
-					return ( $("body").width() - $(".main-poster").width() )/2
-				}
-			})
-		}
-	}
-	
-	$(".main-poster").load(function(){
-		init();
-	})
 
 	init();
 
