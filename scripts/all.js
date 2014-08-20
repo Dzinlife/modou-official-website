02ad383f20640d059737da606783919586cedc77
Joe Wong <dzinlife@me.com>
2014-08-20 14:49:37 +0800

    test

--- scripts/all.js
+++ scripts/all.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
 	FastClick.attach(document.body);
 	$.fx.interval = 16;
-	
-	$(window).resize(function(){
-		init();
-	})
-
-	function init() {
-		
-	}
-
-	init();
-
-
 })
